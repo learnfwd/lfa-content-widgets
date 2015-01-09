548743b414d0027ec13a7c99646570e7c167469e
Andrei Constantinescu <andrei@constantinescu.io>
2015-01-09 13:16:32 +0200

    Add charts mixins and documentation.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-define(['./kenburns', './lightbox'], function () {
+define(['jquery', './kenburns', './lightbox', './chart'], function ($) {
   window.App.book.on('render', function () {
+    $('.progress .progress-bar').progressbar();
   });
 });
